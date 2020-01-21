18fd075b25367107326f819e6c4c0a3056dbb9b6
Azurolu <33943251+Azurolu@users.noreply.github.com>
2020-01-21 14:34:01 +0100

    typo

--- Projet_2/src/projet2/ErreurTerritoireDe.java
+++ Projet_2/src/projet2/ErreurTerritoireDe.java
@@ -3,7 +3,7 @@ package projet2;
 public class ErreurTerritoireDe extends Exception{
 	
 	public ErreurTerritoireDe() {
-		super("Le territoire souhaitant attaqué ne possède qu'un seul dé");
+		super("Le territoire souhaitant attaquer ne possède qu'un seul dé");
 	}
 
 }
