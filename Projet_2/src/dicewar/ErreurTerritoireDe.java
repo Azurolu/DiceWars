85720cb41c33495a4f3062c98bf27401c96004b8
Azurolu <33943251+Azurolu@users.noreply.github.com>
2020-01-22 19:21:57 +0100

    Revert "Add files via upload"

--- Projet_2/src/dicewar/ErreurTerritoireDe.java
+++
@@ -1,9 +0,0 @@
-package projet2;
-
-public class ErreurTerritoireDe extends Exception{
-	
-	public ErreurTerritoireDe() {
-		super("Le territoire souhaitant attaquer ne possède qu'un seul dé");
-	}
-
-}
